78fa42c23d9db22ded557f4c99a0ab43c53a768c
Jude Vigor <judevigor@gmail.com>
2026-04-16 01:52:36 +0000

    Replace seed catalog with production MTN pricing

--- services/product_seed.py
+++ services/product_seed.py
@@ -5,28 +5,51 @@ from models import Product
 
 
 DEFAULT_PRODUCTS = [
-    {"name": "MTN 1GB", "network": "MTN", "data_amount": "1", "retail_price": "7.00"},
-    {"name": "MTN 5GB", "network": "MTN", "data_amount": "5", "retail_price": "24.00"},
-    {"name": "Telecel 2GB", "network": "Telecel", "data_amount": "2", "retail_price": "11.00"},
-    {"name": "Telecel 10GB", "network": "Telecel", "data_amount": "10", "retail_price": "44.00"},
-    {"name": "AirtelTigo 3GB", "network": "AirtelTigo", "data_amount": "3", "retail_price": "15.00"},
-    {"name": "AirtelTigo 8GB", "network": "AirtelTigo", "data_amount": "8", "retail_price": "34.00"},
+    {"name": "MTN 1GB", "network": "MTN", "data_amount": "1", "retail_price": "4.70"},
+    {"name": "MTN 2GB", "network": "MTN", "data_amount": "2", "retail_price": "9.40"},
+    {"name": "MTN 3GB", "network": "MTN", "data_amount": "3", "retail_price": "14.00"},
+    {"name": "MTN 4GB", "network": "MTN", "data_amount": "4", "retail_price": "18.70"},
+    {"name": "MTN 5GB", "network": "MTN", "data_amount": "5", "retail_price": "23.40"},
+    {"name": "MTN 6GB", "network": "MTN", "data_amount": "6", "retail_price": "28.10"},
+    {"name": "MTN 7GB", "network": "MTN", "data_amount": "7", "retail_price": "32.80"},
+    {"name": "MTN 8GB", "network": "MTN", "data_amount": "8", "retail_price": "37.40"},
+    {"name": "MTN 10GB", "network": "MTN", "data_amount": "10", "retail_price": "45.60"},
+    {"name": "MTN 15GB", "network": "MTN", "data_amount": "15", "retail_price": "68.40"},
+    {"name": "MTN 20GB", "network": "MTN", "data_amount": "20", "retail_price": "85.10"},
+    {"name": "MTN 25GB", "network": "MTN", "data_amount": "25", "retail_price": "106.40"},
+    {"name": "MTN 30GB", "network": "MTN", "data_amount": "30", "retail_price": "127.70"},
+    {"name": "MTN 40GB", "network": "MTN", "data_amount": "40", "retail_price": "170.20"},
+    {"name": "MTN 50GB", "network": "MTN", "data_amount": "50", "retail_price": "208.70"},
+    {"name": "MTN 100GB", "network": "MTN", "data_amount": "100", "retail_price": "369.20"},
 ]
 
 
 def seed_products():
-    if Product.query.first():
-        return
+    existing_products = {
+        (product.network, product.data_amount): product
+        for product in Product.query.all()
+    }
+    seeded_keys = set()
 
     for item in DEFAULT_PRODUCTS:
-        db.session.add(
-            Product(
+        key = (item["network"], item["data_amount"])
+        seeded_keys.add(key)
+        product = existing_products.get(key)
+
+        if product is None:
+            product = Product(
                 name=item["name"],
                 network=item["network"],
                 data_amount=item["data_amount"],
-                retail_price=Decimal(item["retail_price"]),
-                is_active=True,
             )
-        )
+            db.session.add(product)
+
+        product.name = item["name"]
+        product.retail_price = Decimal(item["retail_price"])
+        product.is_active = True
+
+    for key, product in existing_products.items():
+        if key not in seeded_keys:
+            product.is_active = False
 
     db.session.commit()
